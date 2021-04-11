1af6983abd1e5427725c92c1b67bf183144d4f65
Frans Engström <frans.engstrom@learnet.se>
2021-04-11 17:46:30 +0200

    add basket item component

---
+++ components/Item/Item.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+const Item = ({item, update}) => {
+
+
+    const [quantity, setQuantity] = useState(item.quantity);
+
+    const handleUpdate = () => {
+
+
+        item.quantity = quantity;
+
+        update(item);
+    }
+
+    return (
+        <div>
+            <p><span>{item.id}  </span>
+                <span>{item.name}  </span>
+                <span>{item.price}  </span>
+                <input onChange={(event) => setQuantity(event.target.value)} value={quantity}/>
+                <button onClick={handleUpdate}>Update</button>
+            </p>
+
+        </div>
+
+    )
+}
+
+export default Item;
