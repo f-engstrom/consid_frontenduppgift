27858452ee48928d9f9b6a87f69275c04936653f
Frans Engström <frans.engstrom@learnet.se>
2021-04-12 20:41:03 +0200

    fix startpage styling

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,57 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import {request} from "../lib/datocms";
 import {HOMEPAGE_QUERY} from "../querys/querys";
-
-
+import ProductCard from "../components/ProductCard/ProductCard";
+import {StructuredText} from "react-datocms";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
 
 
 export async function getStaticProps() {
-    const data = await request({
+    const {startpage, allProducts} = await request({
         query: HOMEPAGE_QUERY,
-        variables: {limit: 10}
+        variables: {nrProducts: 4}
     });
 
+
     return {
-        props: {data}
+        props: {startpage, allProducts}
     };
 }
 
-export default function Home({data}) {
+export default function Home({startpage, allProducts}) {
+
+
+
     return (
-        <div className={styles.container}>
+        <Container >
             <Head>
-                <title>{data.startpage.title}</title>
+                <title>{startpage.title}</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    {data.startpage.title}
-                </h1>
+            
+            <Row>
+               <Col md={12} className="text-center"> <Image style={{width: 900, height: 500}} fluid src={startpage.mainImage.url} alt=""/></Col>
+                <Col md={12} className="text-center" ><h1>{startpage.title}</h1></Col>
+                <Col md={12} className="text-center">
+                    <StructuredText data={startpage.content}/>
+                </Col>
+                
 
-                <p className={styles.description}>
-                    Get started by editing{' '}
-                    <code className={styles.code}>pages/index.js</code>
-                </p>
+            </Row>
+            
+            <Row>
+                {
+                    allProducts.map(product =>
+                        <ProductCard product={product}/>)
+                }
+            </Row>
+           
 
-                <div className={styles.grid}>
-                    <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
 
-                    <a href="https://nextjs.org/learn" className={styles.card}>
-                        <h3>Learn &rarr;</h3>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className={styles.card}
-                    >
-                        <h3>Examples &rarr;</h3>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL with Vercel.
-                        </p>
-                    </a>
-                </div>
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
-                </a>
-            </footer>
-        </div>
+        </Container>
     )
 }
