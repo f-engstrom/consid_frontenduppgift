9095c6a3173c0f51defdffbd41de66c0cd593da2
Frans Engström <frans.engstrom@learnet.se>
2021-04-12 22:02:47 +0200

    add simple confirmation page

---
+++ pages/confirmation.js
@@ -0,0 +1,22 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const confirmation = ()=>{
+    
+    return (
+        
+        <Container>
+            <Row>
+                <Col>
+                    <h1 style={{textAlign :"center"}}>Thank you for shopping!</h1>
+                </Col>
+            </Row>
+            
+        </Container>
+        
+    )
+    
+}
+
+export default confirmation;
