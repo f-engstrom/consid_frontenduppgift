92132cbe2c3f04491e1369977818010045bc750e
Frans Engström <frans.engstrom@learnet.se>
2021-04-12 22:03:40 +0200

    add order checkout page with order summary or message to add to cart

---
+++ pages/checkout.js
@@ -0,0 +1,55 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {connect} from "react-redux";
+import OrderSummary from "../components/OrderSummary/OrderSummary";
+import {clearAllItemsAction} from "../store/actions";
+
+const checkout = ({basket,clearAllItems}) => {
+
+    const {items} = basket;
+
+ 
+console.log("items",items.length)
+    
+    let summary = (<Col><h1 style={{textAlign:"center"}}>Oh no, your cart is empty!</h1></Col>)
+
+    if(items.length > 0){
+        
+        summary = <OrderSummary items={items} clear={clearAllItems} />
+        
+    }
+
+
+
+    return (
+        <Container>
+
+            <Row>
+                <Col>
+
+                </Col>
+            </Row>
+
+            <Row>
+
+                {summary}
+
+            </Row>
+        </Container>
+    )
+
+}
+
+
+const mapStateToProps = state => (
+    {
+        basket: state.basket
+    }
+);
+
+const mapDispatchToProps = {
+    clearAllItems: clearAllItemsAction
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(checkout);
