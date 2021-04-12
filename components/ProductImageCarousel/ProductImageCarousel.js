195395566d0c2e7887c986bbe88d980e7129a09e
Frans Engström <frans.engstrom@learnet.se>
2021-04-12 20:39:19 +0200

    create product image carousel component

--- components/ProductImageCarousel/ProductImageCarousel.js
+++ components/ProductImageCarousel/ProductImageCarousel.js
@@ -1,51 +1,33 @@
+import Carousel from 'react-bootstrap/Carousel';
+import {useState} from "react";
+import ProductCard from "../ProductCard/ProductCard";
 
-
-const  productImageCarousel = ()=> {
+const productImageCarousel = ({images})=> {
     const [index, setIndex] = useState(0);
 
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
+    
 
+    
+    const carouselImages = images.map(image => (
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={image.url}
+                alt={image.title}
+            />
+
+        </Carousel.Item>
+
+    ))
+    
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=373940"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Second slide"
-                />
-
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            {carouselImages}
         </Carousel>
     );
 }
+
+export default productImageCarousel;
