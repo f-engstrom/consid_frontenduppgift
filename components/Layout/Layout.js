b93b2c629234fa23f953455c0368c3c8330cf2bc
Frans Engström <frans.engstrom@learnet.se>
2021-04-12 11:42:57 +0200

    add react boostrap components to Layout.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,18 +1,29 @@
-
 import PageHeader from "../PageHeader/PageHeader";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const layout =(props)=> {
-    
-    console.log("layoutprops",props)
-    
-    return(
-        <div>
-            <PageHeader pages={props.pages}/>
-            {props.children}
-        </div>
-        )
-    
-    
+
+const layout = (props) => {
+
+    console.log("layoutprops", props)
+
+    return (
+
+        <Container fluid>
+            <Row>
+                <Col>
+                    <PageHeader pages={props.pages}/>
+
+                    {props.children}
+
+                </Col>
+            </Row>
+        </Container>
+     
+    )
+
+
 }
 
 export default layout;
