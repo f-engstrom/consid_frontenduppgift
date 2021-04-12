e135334b0c7f17ee2f8b77ad1f39a7a363ab118b
Frans Engström <frans.engstrom@learnet.se>
2021-04-12 20:44:33 +0200

    fix contentpage styling

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,9 +2,12 @@ import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import {request} from "../lib/datocms";
 import {PAGE_QUERY, PAGES_QUERY} from "../querys/querys";
-
-
-
+import {StructuredText} from "react-datocms";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import ProductCard from "../components/ProductCard/ProductCard";
 
 export async function getStaticPaths() {
 
@@ -14,13 +17,11 @@ export async function getStaticPaths() {
         variables: {}
     });
 
-    
-    
+
     const paths = pages.allPages.map((page) => ({
         params: {slug: page.slug},
     }))
 
-    console.log("paths",paths)
 
     return {paths, fallback: false}
 }
@@ -28,27 +29,47 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({params}) {
 
-    console.log("params", params)
 
-    const data = await request({
+    const {page} = await request({
         query: PAGE_QUERY,
         variables: {pageSlug: params.slug}
     });
 
-    console.log(data)
     return {
-        props: {data}
+        props: {page}
     };
 }
 
 
-export default function ContentPage({data}) {
-    
-    console.log("page data",data);
-    
-    
-    
+export default function ContentPage({page}) {
+
+    console.log("page data", page);
+
+
     return (
-        <div>Content Page</div>
+
+        <Container>
+            <Head>
+                <title>{page.title}</title>
+                <link rel="icon" href="https://consid.se/wp-content/uploads/2019/12/Icon-white.svg"/>
+            </Head>
+
+
+            <Row>
+                <Col md={12} className="text-center">
+                    <Image style={{width: 900, height: 500}} fluid
+                           src={page.mainImage.url} alt=""/>
+                </Col>
+                <Col md={12} className="text-center"><h1>{page.title}</h1></Col>
+                <Col md={12} className="text-center">
+                    <StructuredText data={page.content}/>
+                </Col>
+
+
+            </Row>
+
+
+        </Container>
+
     )
 }
